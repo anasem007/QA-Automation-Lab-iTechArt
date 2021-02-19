e47a2ae336640ac54a33ea1f8854c542f9590847
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-02-19 12:57:22 +0300

    Slightly change the code

--- ClassesObjectsMethods/ClassesObjectsMethods/Models/Job.cs
+++ ClassesObjectsMethods/ClassesObjectsMethods/Models/Job.cs
@@ -3,7 +3,9 @@ namespace ClassesObjectsMethods.Models
     public class Job
     {
         public string Title { get; set; }
+        
         public string Description { get; set; }
+       
         public int Salary { get; set; }
     }
 }
