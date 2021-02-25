710728d8c650749960f1aa6200b08632e8142fda
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-02-25 11:23:56 +0300

    create JobFactory

---
+++ ClassesObjectsMethods/ClassesObjectsMethods/Factories/JobFactory.cs
@@ -0,0 +1,20 @@
+using Bogus;
+using ClassesObjectsMethods.Models;
+using ClassesObjectsMethods.Utils;
+
+namespace ClassesObjectsMethods.Factories
+{
+    public class JobFactory
+    {
+        public Job CreateJob()
+        {
+            return new Faker<Job>()
+                .Rules((f, j) =>
+                {
+                    j.Title = f.Name.JobTitle();
+                    j.Description = f.Name.JobDescriptor();
+                    j.Salary = f.Random.Int(Constants.MinSalary, Constants.MaxSalary);
+                });
+        }
+    }
+}
