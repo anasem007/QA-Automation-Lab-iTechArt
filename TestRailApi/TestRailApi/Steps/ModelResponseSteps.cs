2575c49c7f266f1b83513bc16aa0e15c499928b3
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-04-19 23:00:59 +0300

    Add Steps

---
+++ TestRailApi/TestRailApi/Steps/ModelResponseSteps.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using RestSharp;
+using TestRailApi.Helpers;
+using TestRailApi.Models;
+
+namespace TestRailApi.Steps
+{
+    public class ModelResponseSteps<T>
+    {
+        public async Task<IRestResponse<T>> CreateResponse(string endPoint, string requestType,
+            User user, object model = null)
+        {
+            var request = new Request
+            {
+                Uri = endPoint,
+                HttpMethod = requestType,
+                Body = model
+            };
+            
+            var client = ClientHelper.GetClient(user);
+            return ClientHelper.GetResponse<T>(client, request).Result;
+        }
+    }
+}
