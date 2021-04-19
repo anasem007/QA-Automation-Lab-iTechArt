e2e93087739ff883ae50aa7e1ed8d0017c5bc046
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-04-19 22:52:27 +0300

    Add Constants

---
+++ TestRailApi/TestRailApi/Constants/EndPoints.cs
@@ -0,0 +1,11 @@
+namespace TestRailApi.Constants
+{
+    public static class EndPoints
+    {
+        public const string AddProjectEndPoint = "index.php?/api/v2/add_project";
+        public const string GetProjectEndPoint = "index.php?/api/v2/get_project/";
+        public const string DeleteProjectEndPoint = "index.php?/api/v2/delete_project/";
+        public const string AddSuiteEndPoint = "index.php?/api/v2/add_suite/";
+        public const string UpdateSuiteEndPoint = "index.php?/api/v2/add_suite/";
+    }
+}
---
+++ TestRailApi/TestRailApi/Constants/RequestType.cs
@@ -0,0 +1,15 @@
+namespace TestRailApi.Constants
+{
+    public class RequestType
+    {
+        public const string Get = nameof(Get);
+
+        public const string Delete = nameof(Delete);
+
+        public const string Post = nameof(Post);
+
+        public const string Put = nameof(Put);
+
+        public const string Patch = nameof(Patch);
+    }
+}
